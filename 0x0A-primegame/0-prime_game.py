a0a0c59100f990cdc152931ea0010a62b81a9d9f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-03-15 02:59:02 +0000

    fixed doc

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,56 +2,44 @@
 """Module defining isWinner function."""
 
 
-def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True
-
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
+def is_prime(num):
+    if num < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
+def optimal_move(nums):
+    # Sort the list of numbers in reverse order
+    nums.sort(reverse=True)
+    
+    # Iterate through the sorted list
+    for num in nums:
+        # If the number is prime, return it
+        if is_prime(num):
+            return num
+    # If no prime number is found, return None
+    return None
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+def isWinner(x, nums):
+    maria_wins = 0
+    ben_wins = 0
+    
+    for n in nums:
+        # Count the number of prime numbers in the current set
+        prime_count = sum(1 for num in range(1, n + 1) if is_prime(num))
+        
+        # If there are an odd number of primes, Maria wins
+        if prime_count % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the winner based on the number of rounds won
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
